c64d87ce51c0e2dce528bc576726d51879f8c2d5
Katie Simon <katiewsimon@gmail.com>
2015-03-08 03:50:03 -0400

    still struggling with float vs int vs other

--- app/app.py
+++ app/app.py
@@ -11,13 +11,55 @@ def get_recommendation_for_foods():
     food_ids = request.args.get('foods').split(',')
     food_ids = map(int, food_ids)
     foods = mongo.db.food_data.find({"NDB_No": {'$in': food_ids}})
+    nutrients = list(mongo.db.nutrients.find({}))
+    # print(nutrients)
+
 
     response = []
 
-    for food in foods:
-       response[food.get('Shrt_Desc')] = 'ok'
+    # foods is the foods that the user inputs
+    # loop through each food to grab the nutrient amount per nutrient category
+    # sum the nutrient amount in each nutrient category for all the foods
+    # append the nutrient amounts to the response
 
-    response['boom'] = 5
+    nutrients_consumed = {
+    }
+    counter = 0
+    for food in foods:
+        nutrient_amounts = {}
+        food_id = food.get('NDB_No')
+        food_name = food.get('Shrt_Desc')
+
+        # print(nutrients)
+
+        for nutrient in nutrients:
+            nutrient_name = nutrient.get('Nutrient')
+            food_nutrient_val = food.get(nutrient_name)
+            # print(food_name + " : " + nutrient.get('Nutrient') + ' => ' )
+            # print(food.get(nutrient_name))
+            # 'fat' : 'Fat_(mg)'
+            if not nutrient_name in nutrients_consumed:
+                nutrients_consumed[nutrient_name] = food.get(nutrient_name)
+            else:
+                nut_consumed = nutrients_consumed[nutrient_name]
+                food_nut_val = food.get(nutrient_name)
+                # print(food_nut_val)
+                # print(nut_consumed)
+                # print("consumed: ")
+                # print(int(nut_consumed))
+                if nut_consumed is float:
+                    print "nut_consumed is float"
+                # (nut_consumed is float or nut_consumed is int or nut_consumed is None) and (food_nut_val is float or food_nut_val is int or food_nut_val is None):
+                    # print(food_nut_val)
+                    # print(nut_consumed)
+                    nutrients_consumed[nutrient_name] = nut_consumed + food_nut_val
+                else:
+                    # print "not a float"
+                    counter = counter + 1
+        print(counter)
+        response.append(nutrient_amounts)
+
+
 
     #response = [
     #        {
@@ -26,6 +68,10 @@ def get_recommendation_for_foods():
     #            'quantity': 0.8
     #        }
     #]
+    #     nutrients_consumed = {
+    # #     'fat': 0.6,
+    # #     'sodium': 0.8
+    # }
 
     response_json = dumps({"nutrients" : response})
     resp = Response(response_json, status=200, mimetype='application/json')
