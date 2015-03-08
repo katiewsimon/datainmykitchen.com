a8e3aa229aa8e4d4615cc834d82a3d03fff2eed4
Katie Simon <katiewsimon@gmail.com>
2015-03-08 03:51:38 -0400

    adding updated database files

--- data/import_to_mongo.sh
+++ data/import_to_mongo.sh
@@ -1,1 +1,2 @@
 mongoimport --host localhost --db datainmykitchen --collection food_data --file food_data_utf8.csv --type csv --headerline 
+mongoimport --host localhost --db datainmykitchen --collection nutrients --file rec_daily_value.csv --type csv --headerline 
