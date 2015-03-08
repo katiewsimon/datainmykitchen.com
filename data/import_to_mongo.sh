91d94f386f4b60e297d90e196b2fd65eec3a3c18
Michael Garate <mpgarate@gmail.com>
2015-03-07 20:40:43 -0500

    fix typo in mongo import script

--- data/import_to_mongo.sh
+++ data/import_to_mongo.sh
@@ -1,1 +1,1 @@
-mongoimport --host localhost --db datainmykitchen.db --collection food_data --file food_data_utf8.csv --type csv --headerline 
+mongoimport --host localhost --db datainmykitchen --collection food_data --file food_data_utf8.csv --type csv --headerline 
